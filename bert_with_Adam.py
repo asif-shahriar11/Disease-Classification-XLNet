8f4a94badef8d3c926e335524e70064fe6ec7458
Debojit Pandit <100163415+Dip4028@users.noreply.github.com>
2024-02-04 15:17:04 +0600

    Update train

--- bert_with_Adam.py
+++ bert_with_Adam.py
@@ -111,32 +111,76 @@ def loss_fn(outputs, targets):
 optimizer = AdamW(model.parameters(), lr = LEARNING_RATE)
 
 
+# # Training of the model for one epoch
+# def train_model(training_loader, model, optimizer):
+
+#     losses = []
+#     correct_predictions = 0
+#     num_samples = 0
+#     # set model to training mode (activate dropout, batch norm)
+#     model.train()
+#     # initialize the progress bar
+#     loop = tq.tqdm(enumerate(training_loader), total=len(training_loader), 
+#                       leave=True, colour='steelblue')
+#     for batch_idx, data in loop:
+#         ids = data['input_ids'].to(device, dtype = torch.int)
+#         mask = data['attention_mask'].to(device, dtype = torch.int)
+#         token_type_ids = data['token_type_ids'].to(device, dtype = torch.int)
+#         targets = data['targets'].to(device, dtype = torch.float16)
+
+#         # forward
+#         outputs = model(ids, mask, token_type_ids) # (batch,predict)=(32,8)
+#         loss = loss_fn(outputs, targets)
+#         losses.append(loss.item())
+#         # training accuracy, apply sigmoid, round (apply thresh 0.5)
+#         outputs = torch.sigmoid(outputs).cpu().detach().numpy().round()
+#         targets = targets.cpu().detach().numpy()
+#         correct_predictions += np.sum(outputs==targets)
+#         num_samples += targets.size   # total number of elements in the 2D array
+
+#         # backward
+#         optimizer.zero_grad()
+#         loss.backward()
+#         nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+#         # grad descent step
+#         optimizer.step()
+
+#         # Update progress bar
+#         #loop.set_description(f"")
+#         #loop.set_postfix(batch_loss=loss)
+
+#     # returning: trained model, model accuracy, mean loss
+#     return model, float(correct_predictions)/num_samples, np.mean(losses)
+
+
 # Training of the model for one epoch
 def train_model(training_loader, model, optimizer):
-
     losses = []
     correct_predictions = 0
     num_samples = 0
+    total_batches = len(training_loader)
+
     # set model to training mode (activate dropout, batch norm)
     model.train()
-    # initialize the progress bar
-    loop = tq.tqdm(enumerate(training_loader), total=len(training_loader), 
-                      leave=True, colour='steelblue')
-    for batch_idx, data in loop:
-        ids = data['input_ids'].to(device, dtype = torch.int)
-        mask = data['attention_mask'].to(device, dtype = torch.int)
-        token_type_ids = data['token_type_ids'].to(device, dtype = torch.int)
-        targets = data['targets'].to(device, dtype = torch.float16)
+
+    for batch_idx, data in enumerate(training_loader):
+        ids = data['input_ids'].to(device, dtype=torch.int)
+        mask = data['attention_mask'].to(device, dtype=torch.int)
+        token_type_ids = data['token_type_ids'].to(device, dtype=torch.int)
+        targets = data['targets'].to(device, dtype=torch.float16)
 
         # forward
-        outputs = model(ids, mask, token_type_ids) # (batch,predict)=(32,8)
+        outputs = model(ids, mask, token_type_ids)  # (batch,predict)=(32,8)
+        # print(outputs)
         loss = loss_fn(outputs, targets)
+        # print(loss)
         losses.append(loss.item())
         # training accuracy, apply sigmoid, round (apply thresh 0.5)
         outputs = torch.sigmoid(outputs).cpu().detach().numpy().round()
         targets = targets.cpu().detach().numpy()
-        correct_predictions += np.sum(outputs==targets)
-        num_samples += targets.size   # total number of elements in the 2D array
+        correct_predictions += np.sum(outputs == targets)
+        # print(correct_predictions)
+        num_samples += targets.size  # total number of elements in the 2D array
 
         # backward
         optimizer.zero_grad()
@@ -145,13 +189,11 @@ def train_model(training_loader, model, optimizer):
         # grad descent step
         optimizer.step()
 
-        # Update progress bar
-        #loop.set_description(f"")
-        #loop.set_postfix(batch_loss=loss)
+        # Print progress
+        print(f"Batch [{batch_idx+1}/{total_batches}], Loss: {loss.item()}")
 
     # returning: trained model, model accuracy, mean loss
-    return model, float(correct_predictions)/num_samples, np.mean(losses)
-
+    return model, float(correct_predictions) / num_samples, np.mean(losses)
 
 
 def eval_model(validation_loader, model, optimizer):
